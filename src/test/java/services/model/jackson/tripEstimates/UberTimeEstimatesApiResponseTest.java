d21fb2b75427e1dfacff14abb029ee2f03526ee0
Jae Bradley <jae.b.bradley@gmail.com>
2016-03-06 16:19:24 -0500

    Added test for time estimates model object

---
+++ src/test/java/services/model/jackson/tripEstimates/UberTimeEstimatesApiResponseTest.java
@@ -0,0 +1,23 @@
+package services.model.jackson.tripEstimates;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class UberTimeEstimatesApiResponseTest {
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final String uberTimeEstimatesApiResponseFileName = "src/test/resources/services.model.jackson/tripEstimates/UberTimeEstimatesApiResponse.json";
+    private final File uberTimeEstimatesApiResponseFile = new File(uberTimeEstimatesApiResponseFileName);
+
+    @Test
+    public void testUberTimeEstimates() throws IOException {
+        UberTimeEstimatesApiResponse response = mapper.readValue(uberTimeEstimatesApiResponseFile, UberTimeEstimatesApiResponse.class);
+        Assert.assertEquals(response.getTimes().length, 5);
+        Assert.assertEquals(response.getTimes()[0].getProductId(), "55c66225-fbe7-4fd5-9072-eab1ece5e23e");
+        Assert.assertEquals(response.getTimes()[0].getProductName(), "uberX");
+        Assert.assertEquals(response.getTimes()[0].getWaitSeconds(), 120);
+    }
+}
