9930fe4a705dc2531f2ef16b779118ea53dc3681
Jae Bradley <jae.b.bradley@gmail.com>
2016-03-05 20:45:35 -0500

    Added server and resource

---
+++ src/main/java/api/estimates/EstimatesResource.java
@@ -0,0 +1,16 @@
+package api.estimates;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("estimates")
+public class EstimatesResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String foo() {
+        return "Foo";
+    }
+}
