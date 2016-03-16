e53f773e7c06811e293eb51db1088c4e2150c2a0
Jae Bradley <jae.b.bradley@gmail.com>
2016-03-15 22:52:49 -0400

    Updated port value

--- src/main/java/server/App.java
+++ src/main/java/server/App.java
@@ -6,13 +6,15 @@ import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.servlet.ServletContainer;
 
 public class App {
+    public static String PORT = System.getenv("PORT");
+
     public static void main(String[] args) throws Exception {
         ServletHolder sh = new ServletHolder(ServletContainer.class);
         sh.setInitParameter("com.sun.jersey.config.property.resourceConfigClass", "com.sun.jersey.api.core.PackagesResourceConfig");
         sh.setInitParameter("jersey.config.server.provider.packages", "api.estimates");//Set the package where the services reside
         sh.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature", "true");
 
-        Server server = new Server(9999);
+        Server server = new Server(Integer.valueOf(PORT));
         ServletContextHandler context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS);
         context.addServlet(sh, "/*");
         server.start();
